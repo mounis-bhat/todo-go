7655d702741a9ce765bb9ae7eaea22e44437a5fb
Mounis Bhat <mounisbhat@gmail.com>
2022-08-10 20:08:29 +0530

    feat: :bookmark: Remove static routes, use DB
    Breaking changes:
    Sync with postgres DB
    Use Gorm
    Include env variables
    Create todo migration
    Create todo model
    Create CRUD controllers

---
+++ controllers/todoController.go
@@ -0,0 +1,89 @@
+package controllers
+
+import (
+	"example/todo-go/initializers"
+	"example/todo-go/models"
+
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IndexPage(context *gin.Context) {
+	context.HTML(http.StatusOK, "index.html", gin.H{
+		"content": "This is an index page...",
+	})
+}
+
+func CreateTodo(context *gin.Context) {
+
+	var body struct {
+		Item      string
+		Completed bool
+	}
+
+	context.Bind(&body)
+
+	todo := models.Todo{Item: body.Item, Completed: body.Completed}
+	result := initializers.DB.Create(&todo)
+
+	if result.Error != nil {
+		context.Status(400)
+		return
+	}
+
+	context.JSON(200, gin.H{
+		"todo": todo,
+	})
+}
+
+func GetTodoList(context *gin.Context) {
+	var todoList []models.Todo
+
+	initializers.DB.Find(&todoList)
+
+	context.JSON(200, gin.H{
+		"todoList": todoList,
+	})
+}
+
+func GetTodo(context *gin.Context) {
+
+	id := context.Param("id")
+
+	var todo models.Todo
+
+	initializers.DB.First(&todo, id)
+
+	context.JSON(200, gin.H{
+		"todo": todo,
+	})
+}
+
+func UpdateTodo(context *gin.Context) {
+	id := context.Param("id")
+
+	var body struct {
+		Item      string
+		Completed bool
+	}
+
+	context.Bind(&body)
+	var todo models.Todo
+
+	initializers.DB.First(&todo, id)
+	initializers.DB.Model(&todo).Updates(models.Todo{
+		Item: body.Item, Completed: body.Completed,
+	})
+	context.JSON(200, gin.H{
+		"todo": todo,
+	})
+}
+
+func DeleteTodo(context *gin.Context) {
+	id := context.Param("id")
+
+	initializers.DB.Delete(&models.Todo{}, id)
+
+	context.Status(200)
+}
--- main.go
+++ main.go
@@ -1,107 +1,15 @@
 package main
 
 import (
-	"errors"
-	"net/http"
+	"example/todo-go/controllers"
+	"example/todo-go/initializers"
 
 	"github.com/gin-gonic/gin"
 )
 
-type todo struct {
-	ID        string `json:"id"`
-	Item      string `json:"item"`
-	Completed bool   `json:"completed"`
-}
-
-var todoList = []todo{
-	{ID: "1", Item: "Create REST API in go", Completed: false},
-	{ID: "2", Item: "Take a shower", Completed: false},
-	{ID: "3", Item: "Cook pasta", Completed: false},
-}
-
-func getTodoList(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, todoList)
-}
-
-func addTodo(context *gin.Context) {
-	var newTodo todo
-
-	if err := context.BindJSON(&newTodo); err != nil {
-		return
-	}
-
-	todoList = append(todoList, newTodo)
-	context.IndentedJSON(http.StatusCreated, newTodo)
-}
-
-func getTodoById(context *gin.Context) {
-	id := context.Param("id")
-
-	todo, error := searchTodo(id)
-
-	if error != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-		return
-	}
-
-	context.IndentedJSON(http.StatusOK, todo)
-}
-
-func toggleTodoStatus(context *gin.Context) {
-	id := context.Param("id")
-
-	todo, error := searchTodo(id)
-
-	if error != nil {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-		return
-	}
-
-	todo.Completed = !todo.Completed
-	context.IndentedJSON(http.StatusOK, todo)
-}
-
-func deleteTodo(context *gin.Context) {
-	id := context.Param("id")
-
-	found := false
-
-	for index, todo := range todoList {
-		if todo.ID == id {
-			todoList = append(todoList[:index], todoList[index+1:]...)
-			found = true
-			break
-		}
-	}
-
-	if found {
-		context.IndentedJSON(http.StatusOK, gin.H{"message": "Todo deleted"})
-	} else {
-		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-	}
-
-}
-
-func indexPage(context *gin.Context) {
-	context.HTML(http.StatusOK, "index.html", gin.H{
-		"content": "This is an index page...",
-	})
-}
-
-func searchTodo(id string) (*todo, error) {
-	// for i := 0; i < len(todoList); i++ {
-	// 	if todoList[i].ID == id {
-	// 		return &todoList[i], nil
-	// 	}
-	// }
-
-	for index, todo := range todoList {
-		if todo.ID == id {
-			return &todoList[index], nil
-		}
-	}
-
-	return nil, errors.New("todo not found")
+func init() {
+	initializers.LoadEnvVars()
+	initializers.ConnectToDB()
 }
 
 func main() {
@@ -110,12 +18,12 @@ func main() {
 	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("templates/*.html")
 
-	router.GET("/", indexPage)
+	router.GET("/", controllers.IndexPage)
+	router.GET("/todoList", controllers.GetTodoList)
+	router.GET("/todoList/:id", controllers.GetTodo)
+	router.PUT("/todoList/:id", controllers.UpdateTodo)
+	router.POST("/todoList", controllers.CreateTodo)
+	router.DELETE("/todoList/:id", controllers.DeleteTodo)
 
-	router.GET("/todoList", getTodoList)
-	router.GET("/todoList/:id", getTodoById)
-	router.PATCH("/todoList/:id", toggleTodoStatus)
-	router.POST("/todoList", addTodo)
-	router.DELETE("/todoList/:id", deleteTodo)
-	router.Run("localhost:8080")
+	router.Run()
 }
